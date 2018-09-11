710a3a906224ce61458e52e6ca1ab0f51a0bb313
久保卓也 <>
2018-09-12 08:49:42 +0900

    cpの扱いを変えた。

--- run.sh
+++ run.sh
@@ -7,5 +7,8 @@ python /opt/tensorflow/syntaxnet/server.py &
 
 cd /opt/corenlp
 
+export CLASSPATH="$CLASSPATH:javanlp-core.jar:stanford-english-corenlp-2018-02-27-models.jar:stanford-english-kbp-corenlp-2018-02-27-models.jar";
+for file in `find lib -name "*.jar"`; do export CLASSPATH="$CLASSPATH:`realpath $file`"; done
+
 # Start the second process
-java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
+java -mx4g edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
